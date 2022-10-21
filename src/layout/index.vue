62d202e519f6ae19e283d6951ea707181bde2769
Oliver <wang59651@foxmail.com>
2022-10-21 18:45:12 +0800

    feat:抽离侧边

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -4,51 +4,7 @@
       <div class="logo">ZUD3</div>
     </a-layout-header>
     <a-layout>
-      <a-layout-sider width="200" style="background: #fff">
-        <a-menu
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
-          mode="inline"
-          :style="{ height: '100%', borderRight: 0 }"
-        >
-          <a-sub-menu key="sub1">
-            <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
-            </template>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-            </template>
-            <a-menu-item key="5">option5</a-menu-item>
-            <a-menu-item key="6">option6</a-menu-item>
-            <a-menu-item key="7">option7</a-menu-item>
-            <a-menu-item key="8">option8</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-            </template>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
-          </a-sub-menu>
-        </a-menu>
-      </a-layout-sider>
+      <Sider></Sider>
       <a-layout style="padding: 0 24px 24px">
         <a-layout-content
           :style="{
@@ -83,6 +39,7 @@
   </a-layout>
 </template>
 <script setup>
+import Sider from "./sider.vue";
 import ZuSplitPane from "@/components/zu-split-pane/index.vue";
 import ZuCode from "@/components/zu-code/index.vue";
 import ZuMarkdown from "@/components/zu-markdown/index.vue";
@@ -93,11 +50,9 @@ import {
   LaptopOutlined,
   NotificationOutlined,
 } from "@ant-design/icons-vue";
-import {  ref, shallowRef } from "vue";
+import { ref, shallowRef } from "vue";
 let offset = ref(0.5);
 const loading = shallowRef(false);
-let selectedKeys2 = ref(["1"]);
-let openKeys = ref(["sub1"]);
 function move(val) {
   offset.value = val;
 }
---
+++ src/layout/sider.vue
@@ -0,0 +1,74 @@
+<!--
+ * @Author: zulezhe
+ * @Date: 2022-10-21 18:03:57
+ * @LastEditors: zulezhe
+ * @LastEditTime: 2022-10-21 18:43:07
+ * @Path: https://gitee.com/zulezhe/
+ * @Description: 
+-->
+<template>
+  <a-layout-sider width="200" style="background: #fff">
+    <a-menu
+      v-model:selectedKeys="selectedKeys2"
+      v-model:openKeys="openKeys"
+      mode="inline"
+      @click="handelClick"
+      :selectedKeys="[$route.path]"
+      :style="{ height: '100%', borderRight: 0 }"
+    >
+      <a-sub-menu key="sub1">
+        <template #title>
+          <span>
+            <user-outlined />
+            基础
+          </span>
+        </template>
+        <a-menu-item key="/ring-menu">环形菜单</a-menu-item>
+        <a-menu-item key="2">option2</a-menu-item>
+        <a-menu-item key="3">option3</a-menu-item>
+        <a-menu-item key="4">option4</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub2">
+        <template #title>
+          <span>
+            <laptop-outlined />
+            进阶
+          </span>
+        </template>
+        <a-menu-item key="5">option5</a-menu-item>
+        <a-menu-item key="6">option6</a-menu-item>
+        <a-menu-item key="7">option7</a-menu-item>
+        <a-menu-item key="8">option8</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub3">
+        <template #title>
+          <span>
+            <notification-outlined />
+            实战
+          </span>
+        </template>
+        <a-menu-item key="9">option9</a-menu-item>
+        <a-menu-item key="10">option10</a-menu-item>
+        <a-menu-item key="11">option11</a-menu-item>
+        <a-menu-item key="12">option12</a-menu-item>
+      </a-sub-menu>
+    </a-menu>
+  </a-layout-sider>
+</template>
+
+<script setup>
+import { reactive, ref, toRefs } from "vue";
+let selectedKeys2 = ref(["1"]);
+let openKeys = ref(["sub1"]);
+function handelClick(item) {
+  //判断点击路径与点击菜单路径是否不同
+  //有效避免重复替换相同路径
+  if (item.key !== this.$route.path) {
+    this.$router.push(item.key);
+    console.log(this.$route.path);
+    console.log(item);
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
