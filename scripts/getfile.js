6bf9f0f719d49a1de5281a6280bbb6914ab44a6c
Oliver <wang59651@foxmail.com>
2022-10-24 19:06:38 +0800

    feat:增加自动生成菜单脚本

--- scripts/getfile.js
+++ scripts/getfile.js
@@ -2,7 +2,7 @@
  * @Author: zulezhe
  * @Date: 2022-10-24 17:47:00
  * @LastEditors: zulezhe
- * @LastEditTime: 2022-10-24 18:59:44
+ * @LastEditTime: 2022-10-24 19:05:18
  * @Path: https://gitee.com/zulezhe/
  * @Description: 
  */
@@ -12,21 +12,18 @@ import { fileURLToPath } from 'url'
 const __filenameNew = fileURLToPath(import.meta.url)
 const __dirnameNew = path.dirname(__filenameNew)
 const dirPath = path.resolve("public/doc")
-
+let result = []
 function getFile(dirPath) {
-  return new Promise((resolive, reject) => {
-    let result = []
-    let files = fs.readdirSync(dirPath)
-    files.map(finename => {
-      const file = path.join(dirPath, finename)
-      let stats = fs.statSync(file)
-      const isFile = stats.isFile();
-      const isDir = stats.isDirectory();
-      if (isFile) {
-        result.push(file)
-      }
-      if (isDir) getFile(file)
-    })
+  let files = fs.readdirSync(dirPath)
+  files.map(finename => {
+    const file = path.join(dirPath, finename)
+    let stats = fs.statSync(file)
+    const isFile = stats.isFile();
+    const isDir = stats.isDirectory();
+    if (isFile) {
+      result.push(file)
+    }
+    if (isDir) getFile(file)
   })
 }
 function format(result) {
@@ -34,13 +31,17 @@ function format(result) {
   result.map(filePath => {
     let item = filePath.replace(dirPath + '\\', '')
     if (item.includes('.html')) {
+      let file = item.split("\\")
       res.push({
         fname: item.split("\\")[0],
         html: item.split("\\")[1],
       })
     }
   })
+  console.log(res);
+  fs.writeFileSync("public/menu.json", JSON.stringify(res), {
+    encoding: 'utf8',
+  })
 }
-getFile(dirPath).then(result => {
-  format(result)
-})
+getFile(dirPath)
+format(result)
