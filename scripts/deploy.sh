31689b73fa24da9e1810c67d1fe92eca35bc01db
Oliver <wang59651@foxmail.com>
2022-10-25 13:43:04 +0800

    feat:修改

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,47 +1,41 @@
 #!/usr/bin/env bash
-# 發生錯誤時執行終止指令
 ###
  # @Author: zulezhe
  # @Date: 2022-10-24 17:12:42
  # @LastEditors: zulezhe
- # @LastEditTime: 2022-10-24 17:39:58
+ # @LastEditTime: 2022-10-25 13:37:57
  # @Path: https://gitee.com/zulezhe/
  # @Description: 
 ### 
 set -e
 
-# 取得目前專案的遠端分支
+# 获取远程仓库地址
 originUrl=$(git config --get remote.origin.url)
 
-# 注意若是出現以下 Url 就是走 SSH
-# git@github.com:hsiangfeng/HexfootMusic.git
-# 反之，若是 HTTPs 的話則是以下
-# https://github.com/hsiangfeng/HexfootMusic.git
-# 通常來講走 SSH 可能會出現 GitHub Public Keys 錯誤，這部分 Google 一下就可以解決囉。
+echo '远程分支地址为'$originUrl
 
+# 判断是https还是ssh
 nowStatus=${originUrl:0:5}
 echo $nowStatus
 if [ $nowStatus = 'https' ]; then
-  echo '傳輸方式採用 HTTPs 模式'
-  echo '目前遠端分支 URL：'$originUrl
+  echo 'HTTPs 模式'
+  echo '远程 URL：'$originUrl
 else
-  echo '傳輸方式採用 SSH 模式'
-  echo '目前遠端分支 URL：'$originUrl
+  echo ' SSH 模式'
+  echo '远程 URL：'$originUrl
 fi
 
 # 打包編譯
 npm run build
 
-echo '移動目錄到編譯出來的 dist 資料夾'
-#移動到打包資料夾下，若你有調整的話打包後的資料夾請務必調整
+echo '进入dist目录'
 cd dist
-# 初始化並設置 Git
+# 执行git命令
 git init
 git add .
 git commit -m "update $(date +'%Y-%m-%d %H:%M:%S')"
 
-# 上傳到到 gh-pages
-# 如果是走 SSH 的話可能會比較不好調整
+# 上传到 gh-pages
 git push -f $originUrl master:gh-pages
 
 cd -
